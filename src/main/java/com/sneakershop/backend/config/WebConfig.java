3e8b6783e3bb7e7076e294248677548683cd80e9
HieuWest <ndhieu1610@gmail.com>
2026-01-12 00:46:33 +0700

    update connect fe

---
+++ src/main/java/com/sneakershop/backend/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.sneakershop.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Cho phép tất cả các đường dẫn bắt đầu bằng /api
+                .allowedOrigins("http://localhost:5173") // Link của Frontend Vue.js
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Các phương thức cho phép
+                .allowedHeaders("*");
+    }
+}
---
+++ src/main/java/com/sneakershop/backend/config/abcabc.java
@@ -0,0 +1,4 @@
+package com.sneakershop.backend.config;
+
+public class abcabc {
+}
